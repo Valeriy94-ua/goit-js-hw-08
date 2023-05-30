3a1be1408ca7195834dc545864fc0e5482a8f72d
Valeriy94-ua <128257210+Valeriy94-ua@users.noreply.github.com>
2023-05-30 18:06:40 +0300

    upd task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,9 @@
 import localstorageAPI from './localstorage.js';
-
 const throttle = require('lodash.throttle');
 
 const inputForm = document.querySelector('form');
 inputForm.classList.add('js-form-input');
-const userInfo = {};
+let userInfo = {};
 
 const fillInputFromLocalStorage = () => {
   const userInfoFromLS = localstorageAPI.load('feedback-form-state');
@@ -15,12 +14,17 @@ const fillInputFromLocalStorage = () => {
 
   for (let key in userInfoFromLS) {
     inputForm.elements[key].value = userInfoFromLS[key];
+    if (userInfoFromLS[key] !== '') {
+      userInfo[key] = userInfoFromLS[key];
+    }
+    console.log(userInfo);
   }
 };
 
 fillInputFromLocalStorage();
 
 const formOnChange = event => {
+  console.log(userInfo);
   const inputEmailEl = event.target;
   const inputEmailValue = inputEmailEl.value;
   const inputEmailName = inputEmailEl.name;
@@ -29,12 +33,22 @@ const formOnChange = event => {
   localstorageAPI.save('feedback-form-state', userInfo);
 };
 
+// check fields on content
+
 const onFormElSubmit = event => {
   event.preventDefault();
 
+  const inputEmail = document.querySelector('input');
+  const inputMessage = document.querySelector('textarea');
+  if (inputEmail.value === '' || inputMessage.value === '') {
+    return; // Нічого не робимо, якщо одне з полів порожнє
+  }
+  console.log(inputEmail.value);
   const inputForm = event.target;
+
   inputForm.reset();
   localstorageAPI.remove('feedback-form-state');
+  userInfo = {};
 };
 
 inputForm.addEventListener('input', throttle(formOnChange, 500));
